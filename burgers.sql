a51e0f405f7b600e4763f7d5818272c1b518ff4b
m-fiks <fiksd005@umn.edu>
2018-07-24 19:35:19 -0500

    added travis

---
+++ burgers.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Basic Burger", true),
+("Cheesy Burger", true),
+("Hamsterdam Burger", false),
+("Baconator Burger", false)
